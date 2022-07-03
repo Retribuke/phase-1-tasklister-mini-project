15b42fe56035d019707475f9f5295caa67e266ce
Justyn Enyart <gweeners17@gmail.com>
2022-07-03 11:03:59 -0400

    added completion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    addToDo(e.target.new_todo.value);
+    });
 });
+
+//Adding todo function
+function addToDo(todo) {
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.textContent = "Delete";
+  btn.addEventListener('click', () => {
+    btn.parentElement.remove();
+  })
+  li.textContent = ` ${todo} `;
+  li.appendChild(btn);
+  document.getElementById('list').appendChild(li);
+}
